3c0913d6f35610536670ebbc510bdffd1b27d1d3
Matt Jay <146891375+ThomasJNewton@users.noreply.github.com>
2024-01-26 17:55:51 +0100

    Add files via upload

---
+++ Hotel/Hotel/ObsługaMuzyki.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+
+namespace Hotel
+{
+    public static class ObsługaMuzyki
+    {
+        public static void GrajMuzykeWTle(string filePath)
+        {
+            // "Paul Gonsalves - Low Gravy.wav"
+            SoundPlayer musicPlayer = new SoundPlayer();
+            musicPlayer.SoundLocation = filePath;
+            musicPlayer.PlayLooping();
+        }
+
+    }
+}
